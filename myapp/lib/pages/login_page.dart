40eb624dab5222f091a5ec75a16c1e0f21484224
Rony Fernandes <chriscric17@gmail.com>
2025-01-24 18:53:57 +0530

    made LoginPage

--- myapp/lib/pages/login_page.dart
+++ myapp/lib/pages/login_page.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/components/my_button.dart';
+import 'package:myapp/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // LOGO
+            Icon(
+              Icons.lock_open,
+              size: 100.0,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            const SizedBox(height: 25),
+
+            // Message
+            Text(
+              "Food Delivery App",
+              style: TextStyle(
+                  fontSize: 16.0,
+                  color: Theme.of(context).colorScheme.inversePrimary),
+            ),
+            const SizedBox(height: 25),
+            // Email textfield
+            MyTextfield(
+              controller: emailController,
+              hintText: "Email",
+              obscureText: false,
+            ),
+            const SizedBox(height: 10),
+            MyTextfield(
+              controller: passwordController,
+              hintText: "Password",
+              obscureText: true,
+            ),
+            const SizedBox(height: 10),
+
+            // Sign In Button
+            MyButton(text: "Sign In", onTap: () {})
+
+                        const SizedBox(height: 10),
+
+          ],
+        ),
+      ),
+    );
   }
 }
